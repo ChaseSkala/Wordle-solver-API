694fdbf7a4dd94a9c0baed0efb2f877e1c760426
chaseskala <Chase@cgcg.biz>
2025-06-09 19:12:49 -0400

    New history response that displays every word guessed and the status of each letter for that guess.

--- main.py
+++ main.py
@@ -6,19 +6,27 @@ app = FastAPI()
 
 class GuessRequest(BaseModel):
     guess: str
+
+class GuessResponse(BaseModel):
+    solution: str
+    attempts: list
 @app.get("/")
 def read_root():
-    word, attempts = solve_wordle(None)
-    return {"The wordle is ": f"{word}"
-            f" and it took {attempts} attempts."}
+    word, attempts, history = solve_wordle(None)
+    return {
+        "solution": word,
+        "attempts": attempts,
+        "history": history.to_dict()
+    }
 
 @app.post("/guess")
 def make_guess(data: GuessRequest):
-    guess = data.guess.lower()
-    word, attempts = solve_wordle(guess)
-    return {"The wordle is ": f"{word} and it took {attempts} attempts."}
-
-
+    word, attempts, history = solve_wordle(data.guess.lower())
+    return {
+        "solution": word,
+        "attempts": attempts,
+        "history": history.to_dict()
+    }
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="127.0.0.1", port=8001)
